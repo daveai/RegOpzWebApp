44989f3a82b9cbe0fba8f06545d3048fb44606b2
Dave <dave@agileinf.com>
2017-03-18 00:23:43 +0530

    previous grid

--- src/components/RegOpzDataGrid/RegOpzDataGridHeader.js
+++ src/components/RegOpzDataGrid/RegOpzDataGridHeader.js
@@ -4,7 +4,7 @@ export default class RegOpzDataGridHeader extends Component {
     constructor(props) {
         super(props);
         this.columns = [];
-        for (let i = 0; i < 1000; i++) {
+        for (let i = 0; i < 50; i++) {
             this.columns.push(this.alphaSequence(i));
         }
         this.regHeaderStyle = {
@@ -12,22 +12,48 @@ export default class RegOpzDataGridHeader extends Component {
         }
         this.colStyle = [];
         this.rows = [
-            {cell:'A1', value:"Sample"},
+            {cell:'A1', value:"Sample", "merged":"V1"},
             {cell:'B1', value:"Sample"},
             {cell:'G4', value:"Sample"}
         ]
         this.matrix = [[]];
-        for(let k = 0; k < this.rows.length; k++){            
+        for(let k = 0; k < 500; k++){
             this.matrix.push([]);
             for(let j = 0; j < this.columns.length; j++){
-                this.matrix[k][j] = k + "," + j;
+                this.matrix[k][j] = {
+                    value:k + "," + j,
+                    stylex:{
+                        width:"100px",
+                        height:"21px"
+                    }
+                }
             }
         }
-        /*this.rows.map(function(item,index){
+        this.rows.map(function(item,index){
             let cell = item.cell;
+            let merged =  item.merged;
             let coord = this.getRealCoords(cell);
-            this.matrix[coord.y][coord.x] = item.value;
-        }.bind(this));*/
+            if(merged){
+                let merged_coord = this.getRealCoords(merged);
+                let y_dist = Math.abs(merged_coord.y - coord.y);
+                let x_dist = Math.abs(merged_coord.x - coord.x);
+                console.log("Distance: ",x_dist,y_dist);
+                for(let i = coord.y + 1; i < y_dist + 1; i++){
+                    this.matrix[i][coord.x].value = "merged";
+                }
+                for(let i = coord.x + 1; i < x_dist + 1; i++){
+                    this.matrix[coord.y][i].value = "merged";
+                }
+                this.matrix[coord.y][coord.x].value = item.value;
+                this.matrix[coord.y][coord.x].stylex.width = (x_dist + 1) * 98;
+                this.matrix[coord.y][coord.x].stylex.height = (y_dist + 1) * 19;
+            } else {
+                if(this.matrix[coord.y][coord.x].value != "merged"){
+                    this.matrix[coord.y][coord.x].value = item.value;
+                }
+            }
+
+        }.bind(this));
     }
     render() {
         var _self = this;
@@ -36,12 +62,7 @@ export default class RegOpzDataGridHeader extends Component {
                 <div className="reg_col_first"></div>
                 {this.columns.map(function(item, index) {
                     return (
-                        <div onClick={(event)=>{
-                                _self.colStyle["reg_resize" + index] = {
-                                    width:"300px"
-                                }
-                                _self.setState({});
-                            }} key={index} className="reg_col" style={_self.colStyle["reg_resize" + index]}>
+                        <div key={index} className="reg_col">
                             <span>{item}</span>
                         </div>
                     )
@@ -57,11 +78,14 @@ export default class RegOpzDataGridHeader extends Component {
                                 <div className="reg_col_first">{rindex}</div>
                                 {
                                     row.map(function(col,cindex){
-                                        return(
-                                            <div key={cindex} className="reg_col" style={_self.colStyle["reg_resize" + cindex]}>
-                                                <span>{col}</span>
-                                            </div>
-                                        )
+                                        if(col.value != "merged"){
+                                            return(
+                                                <div key={cindex} className="reg_col">
+                                                    <span style={col.stylex} >{col.value}</span>
+                                                </div>
+                                            )
+                                        }
+
                                     }.bind(_self))
                                 }
                             </div>
