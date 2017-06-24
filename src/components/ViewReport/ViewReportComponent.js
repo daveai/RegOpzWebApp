4f73d82888b7a7ea487cd5546bc7dddcedbefdfa
Progyan Bhattacharya <progyanb@acm.org>
2017-06-24 15:29:20 +0530

    Report: Redefined Report Tree
    * Redefined Report Tree Component and View Report Component
    * Removed dependencies from View Data Component
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- src/components/ViewReport/ViewReportComponent.js
+++ src/components/ViewReport/ViewReportComponent.js
@@ -7,11 +7,6 @@ import _ from 'lodash';
 import Collapsible from '../CollapsibleModified/Collapsible';
 import ReportTreeInfoComponent from './ReportTreeInfoComponent';
 import {
-  ViewSourceComponent,
-  mapDispatchToProps,
-  mapStateToProps
-} from '../SourceTreeInfo/ViewSourceComponent';
-import {
   actionFetchDates,
   actionFetchReportFromDate,
   actionFetchSource,
@@ -32,13 +27,62 @@ require('react-datepicker/dist/react-datepicker.css');
 require('../../../node_modules/react-treeview/react-treeview.css');
 require('../ViewData/ViewDataComponentStyle.css');
 
-class ViewReportComponent extends ViewSourceComponent {
+class ViewReportComponent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      sources: null
+    }
+    this.dataSource = null;
   }
 
   componentWillMount() {
     this.props.fetchDates(this.state.startDate ? moment(this.state.startDate).format('YYYYMMDD') : "19000101",this.state.endDate ? moment(this.state.endDate).format('YYYYMMDD') : "30200101", 'report_catalog');
+  }
+
+  render() {
+    return(
+      <div className="container view_data_container">
+        <div className="col col-lg-6">
+          <div className="row">
+            <div className="input-group">
+              <DatePicker
+                  selected={this.state.startDate}
+                  onChange={this.handleStartDateChange.bind(this)}
+                  placeholderText="Select start date"
+                  className="view_data_date_picker_input form-control"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col col-lg-6">
+          <div className="row">
+            <div className="input-group">
+              <DatePicker
+                  selected={this.state.endDate}
+                  onChange={this.handleEndDateChange.bind(this)}
+                  placeholderText="Select end date"
+                  className="view_data_date_picker_input form-control"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="clear"></div>
+        {this.renderAccordions()}
+      </div>
+    )
+  }
+
+  handleStartDateChange(date) {
+    this.setState({startDate: date});
+    this.props.fetchDates(date ? moment(date).format('YYYYMMDD') : "19000101",this.state.endDate ? moment(this.state.endDate).format('YYYYMMDD') : "30200101",'report_catalog');
+  }
+
+  handleEndDateChange(date) {
+    this.setState({endDate: date});
+    this.props.fetchDates(this.state.startDate ? moment(this.state.startDate).format('YYYYMMDD') : "19000101",date ? moment(date).format('YYYYMMDD') : "30200101",'report_catalog');
   }
 
   renderAccordions() {
@@ -72,7 +116,6 @@ class ViewReportComponent extends ViewSourceComponent {
                                     month={item}
                                     date={date_item}
                                     key={date_index}
-                                    /*apiFor={this.props.apiFor}*/
                                   />
                                 )
                               })
@@ -90,17 +133,46 @@ class ViewReportComponent extends ViewSourceComponent {
       )
     }
   }
+}
 
-  handleStartDateChange(date) {
-    this.setState({startDate: date});
-    this.props.fetchDates(date ? moment(date).format('YYYYMMDD') : "19000101",this.state.endDate ? moment(this.state.endDate).format('YYYYMMDD') : "30200101",'report_catalog');
-  }
-
-  handleEndDateChange(date) {
-    this.setState({endDate: date});
-    this.props.fetchDates(this.state.startDate ? moment(this.state.startDate).format('YYYYMMDD') : "19000101",date ? moment(date).format('YYYYMMDD') : "30200101",'report_catalog');
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchDates: (startDate, endDate, table_name) => {
+      dispatch(actionFetchDates(startDate, endDate, table_name))
+    },
+    fetchReportFromDate: (source_id, business_date, page) => {
+      dispatch(actionFetchReportFromDate(source_id, business_date, page))
+    },
+    fetchSource: (business_date) => {
+      dispatch(actionFetchSource(business_date))
+    },
+    fetchReportLinkage: (id, qualifying_key, business_date) => {
+      dispatch(actionFetchReportLinkage(id, qualifying_key, business_date));
+    },
+    insertSourceData: (data, at) => {
+      dispatch(actionInsertSourceData(data,at));
+    },
+    updateSourceData: (data) => {
+      dispatch(actionUpdateSourceData(data));
+    },
+    deleteFromSourceData: (id, business_date, table_name, at) => {
+      dispatch(actionDeleteFromSourceData(id, business_date, table_name, at));
+    },
+    fetchDatesForReport: (startDate, endDate) => {
+      dispatch(actionFetchDatesForReport(startDate,endDate))
+    },
   }
 }
+
+function mapStateToProps(state) {
+  console.log("On mapState ", state.view_data_store);
+  return {
+    data_date_heads: state.view_data_store.dates,
+    report: state.report_store,
+    report_linkage: state.view_data_store.report_linkage
+  }
+}
+
 
 const VisibleViewReportComponent = connect(
   mapStateToProps,
