ccb6a206fc05eb5290a99985a1acc6ee9274d24c
Progyan Bhattacharya <progyanb@acm.org>
2017-07-21 15:47:51 +0530

    Breadcrumb: Added name property to each route
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

---
+++ src/components/Home.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {
+    Router,
+    Route,
+    Link,
+    IndexRoute,
+    hashHistory,
+    browserHistory
+} from 'react-router';
+import Dashboard from './Dashboard/Dashboard';
+import CaptureReportTemplate from './CaptureReportTemplate/CaptureReportTemplate';
+import DashboardIndex from './Dashboard/DashBoardIndex';
+import MaintainBusinessRules from './MaintainBusinessRules/MaintainBusinessRules';
+import AddBusinessRule from './MaintainBusinessRules/AddBusinessRule/AddBusinessRule';
+import custom from '../../js/custom';
+import RegOpzDataGrid from './RegOpzDataGrid/RegOpzDataGrid';
+import ViewDataComponentV2 from './ViewData/ViewDataComponentV2';
+import ViewDataComponent from './ViewData/ViewDataComponent';
+import ViewReport from './ViewReport/ViewReport';
+import DrillDown from './DrillDown/DrillDown';
+import MaintainReportRules from './MaintainReportRules/MaintainReportRules';
+import AddReportRules from './MaintainReportRules/AddReportRules/AddReportRules';
+import AddReportAggRules from './MaintainReportRules/AddReportAggRules';
+import MaintainSources from './MaintainSources/MaintainSources';
+import AddSources from './MaintainSources/AddSources/AddSources';
+import VarianceAnalysisForm from './VarianceAnalysis/VarianceAnalysis';
+import VarianceAnalysisGrid from './VarianceAnalysis/VarianceAnalysisGrid';
+import VarianceAnalysisChart from './VarianceAnalysis/VarianceAnalysisChart';
+import CreateReport from './CreateReport/CreateReport';
+import ManageDefChange from './ManageDefChange/ManageDefChange';
+import ManageRoles from './ManageRoles/ManageRoles';
+import AddRoles from './ManageRoles/AddRoles/AddRoles';
+import ManageUsers from './ManageUsers/ManageUsers';
+
+class HomeIndex extends Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="dashboard" name="Dashboard" component={Dashboard} >
+                    <IndexRoute name="Home" component={DashboardIndex} />
+                    <Route path="capture-report-template" name="Capture Report Template" component={CaptureReportTemplate} />
+                    <Route path="data-grid" name="Data Grid" component={RegOpzDataGrid} />
+                    <Route path="maintain-business-rules" name="Maintain Business Rules" component={MaintainBusinessRules} />
+                    <Route path="maintain-business-rules/add-business-rule" component={AddBusinessRule} />
+                    <Route path="view-data" name="View Data" component={ViewDataComponentV2} />
+                    <Route path="view-data-on-grid" name="View Data Grid" component={ViewDataComponent} />
+                    <Route path="view-report" name="View Report" component={ViewReport} />
+                    <Route path="create-report" name="Create Report" component={CreateReport} />
+                    <Route path="drill-down" name="DrillDown" component={DrillDown} />
+                    <Route path="maintain-report-rules" name="Maintain Report Rules" component={MaintainReportRules} />
+                    <Route path="maintain-report-rules/add-report-rules" name="Add Report Rules" component={AddReportRules} />
+                    <Route path="maintain-report-rules/add-report-agg-rules" name="Add Report Aggregate Rules" component={AddReportAggRules} />
+                    <Route path="maintain-sources" name="Maintain Sources" component={MaintainSources} />
+                    <Route path="maintain-sources/add-sources" name="Add Sources" component={AddSources} />
+                    <Route path="variance-analysis" name="Variance Analysis" component={VarianceAnalysisForm} />
+                    <Route path="variance-analysis/variance-data-grid" name="Variance Analysis Grid" component={VarianceAnalysisGrid} />
+                    <Route path="variance-analysis/variance-chart" name="Variance Analysis Chart" component={VarianceAnalysisChart}/>
+                    <Route path="workflow/manage-def-change" name="Manage Definition Change" component={ManageDefChange} />
+                    <Route path="manage-roles" name="Role Management" component={ManageRoles}/>
+                    <Route path="manage-roles/add-roles" name="Add Role" component={AddRoles}/>
+                    <Route path="manage-users" name="User Management" component={ManageUsers}/>
+                </Route>
+            </Router>
+        );
+    }
+}
+
+export default HomeIndex;
--- src/components/ManageRoles/ManageRoles.js
+++ src/components/ManageRoles/ManageRoles.js
@@ -16,9 +16,9 @@ class ManageRolesComponent extends Component {
     super(props);
     this.dataSource = null;
     this.state = {
-        checked:"checked"
+        checked: "checked"
     };
-    this.fetchFlag=true;
+    this.fetchFlag = true;
   }
 
   componentWillMount() {
@@ -32,7 +32,7 @@ class ManageRolesComponent extends Component {
   }
 
   componentDidUpdate(){
-    this.fetchFlag=!this.fetchFlag;
+    this.fetchFlag =! this.fetchFlag;
   }
 
   componentDidMount() {
