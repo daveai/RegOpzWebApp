c975857ae40ec9dd9c3184f9e84991c3c39c87e4
Rud156 <rudrangshu_1997@yahoo.com>
2017-07-31 02:49:04 +0530

    Added Sticky grids and added column selection TODO: Implement the saving of the columns

---
+++ src/components/MaintainBusinessRules/ShowToggleColumns.js
@@ -0,0 +1,173 @@
+import React, { Component } from 'react';
+import { FormControl, Checkbox, Button } from 'react-bootstrap';
+
+class ShowToggleColumns extends Component {
+    constructor(props) {
+        super(props);
+
+        var columns = [];
+        for (let i = 0; i < this.props.columns.length; i++) {
+            let columnObject = {
+                name: this.props.columns[i],
+                index: i,
+                checked: false
+            }
+            columns.push(columnObject);
+        }
+
+        this.state = {
+            value: '',
+            columns: columns,
+            filteredColumns: columns
+        };
+
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleCheckBoxChange = this.handleCheckBoxChange.bind(this);
+        this.selectAllColumns = this.selectAllColumns.bind(this);
+        this.deselectAllColumns = this.deselectAllColumns.bind(this);
+        this.saveCheckSelection = this.saveCheckSelection.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        var columns = [];
+        for (let i = 0; i < nextProps.columns.length; i++) {
+            let columnObject = {
+                name: nextProps.columns[i],
+                index: i,
+                checked: false
+            }
+            columns.push(columnObject);
+        }
+
+        this.setState({
+            value: '',
+            columns: columns,
+            filteredColumns: columns
+        });
+    }
+
+    handleSearchChange(event) {
+        let value = event.target.value;
+        let length = value.length;
+        let filteredColumns = this.state.columns.filter(element => {
+            return (
+                element.name.substring(0, length) === value
+            );
+        });
+
+        this.setState({
+            value: value,
+            filteredColumns: filteredColumns
+        });
+    }
+
+    handleCheckBoxChange(event, index) {
+        var checked = event.target.checked;
+        var mainIndex = this.state.filteredColumns[index].index;
+
+        var filteredColumns = this.state.filteredColumns;
+        var columns = this.state.columns;
+
+        filteredColumns[index].checked = checked;
+        columns[mainIndex].checked = checked;
+
+        this.setState({
+            filteredColumns: filteredColumns,
+            columns: columns
+        });
+    }
+
+    selectAllColumns() {
+        let columns = this.state.columns;
+        let filteredColumns = this.state.filteredColumns;
+
+        for (let i = 0; i < columns.length; i++)
+            columns[i].checked = true;
+        for (let i = 0; i < filteredColumns.length; i++)
+            filteredColumns[i].checked = true;
+
+        this.setState({
+            columns: columns,
+            filteredColumns: filteredColumns
+        });
+    }
+
+    deselectAllColumns() {
+        let columns = this.state.columns;
+        let filteredColumns = this.state.filteredColumns;
+
+        for (let i = 0; i < columns.length; i++)
+            columns[i].checked = false;
+        for (let i = 0; i < filteredColumns.length; i++)
+            filteredColumns[i].checked = false;
+
+        this.setState({
+            columns: columns,
+            filteredColumns: filteredColumns
+        });
+    }
+
+    saveCheckSelection() {
+        // console.log(this.state.columns);
+        this.props.saveSelection(this.state.columns);
+    }
+
+    render() {
+        return (
+            <div>
+                <FormControl
+                    type="text"
+                    placeholder="Enter a search term..."
+                    value={this.state.value}
+                    onChange={this.handleSearchChange}
+                />
+                <br />
+                <div>
+                    <Button
+                        style={{ margin: '14px' }}
+                        onClick={this.selectAllColumns}
+                    >
+                        Select All
+                    </Button>
+                    <Button
+                        style={{ margin: '14px' }}
+                        onClick={this.deselectAllColumns}
+                    >
+                        DeSelect All
+                    </Button>
+                </div>
+                <div style={{ padding: '14px', overflow: 'auto' }}>
+                    {
+                        this.state.filteredColumns.map((element, index) => {
+                            return (
+                                <span
+                                    key={element.index}
+                                    style={{ float: 'left', padding: '7px', display: 'block' }}>
+                                    <input
+                                        style={{ margin: '7px' }}
+                                        type="checkbox"
+                                        onChange={(event) => {
+                                            this.handleCheckBoxChange(event, index)
+                                        }}
+                                        checked={element.checked}
+                                    />
+                                    {element.name}
+                                </span>
+                            );
+                        })
+                    }
+                </div>
+                <div style={{ padding: '14px' }}>
+                    <Button
+                        onClick={this.saveCheckSelection}
+                    >
+                        Save Selection
+                    </Button>
+                </div>
+            </div >
+        )
+    }
+
+}
+
+export default ShowToggleColumns;
