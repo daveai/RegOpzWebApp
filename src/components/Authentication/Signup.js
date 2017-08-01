ecdc2ae2447721d289f6868cb9c348fb1e0d79a1
Progyan Bhattacharya <progyanb@acm.org>
2017-08-01 15:15:18 +0530

    SignUp: Refactored Code
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -4,27 +4,23 @@ import { connect } from 'react-redux';
 import { bindActionCreators, dispatch } from 'redux';
 import { actionAddUser, actionFetchUsers } from '../../actions/UsersAction';
 
-const renderField = ({ input, label, type, meta: { touched, error }}) => {
-
-  return(
-      <div className="form-group">
-        <label className="control-label col-md-3 col-sm-3 col-xs-12">
-          {label}
-        </label>
-        <div className="col-md-9 col-sm-9 col-xs-12">
-          <input {...input} placeholder={label} type={type} className="form-control col-md-4 col-xs-12"/>
-          {
+const renderField = ({ input, label, type, meta: { touched, error }}) => (
+    <div className="form-group">
+      <label className="control-label col-md-3 col-sm-3 col-xs-12">
+        { label }
+      </label>
+      <div className="col-md-9 col-sm-9 col-xs-12">
+        <input {...input} placeholder={label} type={type} className="form-control col-md-4 col-xs-12"/>
+        {
             touched &&
             ((error &&
-              <div className="alert alert-danger">
-                { error }
-              </div>))
-          }
-        </div>
+             <div className="alert alert-danger">
+               { error }
+             </div>))
+        }
       </div>
-      );
-
-}
+    </div>
+);
 
 const asyncValidate = (values, dispatch) => {
   return dispatch(actionFetchUsers(values.name))
@@ -41,130 +37,130 @@ const asyncValidate = (values, dispatch) => {
 const normaliseContactNumber = value => value && value.replace(/[^\d]/g, '')
 
 const validate = (values) => {
-   const errors = {};
+    const errors = {};
 
-   if (!values.name) {
-     errors.name = "User name is required";
-   } else if (values.name.length < 5 || values.name.length > 20 ) {
-     errors.name = "User name must be greater than 4 characters and less than 20 characters";
-   }
+    if (!values.name) {
+        errors.name = "User name is required";
+    } else if (values.name.length < 5 || values.name.length > 20 ) {
+        errors.name = "User name must be greater than 4 characters and less than 20 characters";
+    }
 
-   if (!values.email) {
-    errors.email = 'Email address is required'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address'
-  }
+    if (!values.email) {
+        errors.email = 'Email address is required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
 
-  if (!values.first_name) {
-    errors.first_name = "First name is required"
-  }
+    if (!values.first_name) {
+        errors.first_name = "First name is required"
+    }
 
-  if (!values.last_name) {
-    errors.last_name = "Last name is required"
-  }
+    if (!values.last_name) {
+        errors.last_name = "Last name is required"
+    }
 
-  if (!values.password) {
-      errors.password = "Password should not be empty"
-  }
+    if (!values.password) {
+        errors.password = "Password should not be empty"
+    }
 
-  if (!values.passwordConfirm || values.password !== values.passwordConfirm) {
-    errors.passwordConfirm = "Password must match"
-  }
+    if (!values.passwordConfirm || values.password !== values.passwordConfirm) {
+        errors.passwordConfirm = "Password must match"
+    }
 
-  return errors;
+    return errors;
 }
 
 class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
-
-  componentDidMount() {
-      document.body.classList.add('signup');
-      document.title = "RegOpz Signup";
-  }
-
-  render() {
-    const { handleSubmit, pristine, reset, submitting, message } = this.props;
-    if (message) {
-        return(<div>{ message.msg }</div>);
+    constructor(props) {
+        super(props);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
-    return(
-      <div className="row">
-        <div className="col col-lg-12">
-          <div className='x_panel'>
 
-          <div className="x_content">
-              <form className="form-horizontal form-label-left" onSubmit={ handleSubmit(this.handleFormSubmit) } >
-                  <Field
-                    name="name"
-                    type="text"
-                    component={renderField}
-                    label="Username"
-                  />
-                  <Field
-                      name="first_name"
+    componentDidMount() {
+        document.body.classList.add('signup');
+        document.title = "RegOpz Signup";
+    }
+
+    render() {
+        const { handleSubmit, pristine, reset, submitting, message } = this.props;
+        if (message) {
+            return(<div>{ message.msg }</div>);
+        }
+        return(
+          <div className="row">
+            <div className="col col-lg-12">
+              <div className='x_panel'>
+
+              <div className="x_content">
+                <form className="form-horizontal form-label-left" onSubmit={ handleSubmit(this.handleFormSubmit) } >
+                    <Field
+                      name="name"
                       type="text"
                       component={renderField}
-                      label="First name"
+                      label="Username"
                     />
-
                     <Field
-                      name="last_name"
-                      type="text"
-                      component={renderField}
-                      label="Last name"
-                    />
+                        name="first_name"
+                        type="text"
+                        component={renderField}
+                        label="First name"
+                      />
 
-                    <Field
-                      name="contact_number"
-                      type="text"
-                      component={renderField}
-                      label="Contact number"
-                      normalize={normaliseContactNumber}
-                    />
+                      <Field
+                        name="last_name"
+                        type="text"
+                        component={renderField}
+                        label="Last name"
+                      />
 
-                    <Field
-                      name="email"
-                      type="email"
-                      component={renderField}
-                      label="Email"
-                    />
+                      <Field
+                        name="contact_number"
+                        type="text"
+                        component={renderField}
+                        label="Contact number"
+                        normalize={normaliseContactNumber}
+                      />
+
+                      <Field
+                        name="email"
+                        type="email"
+                        component={renderField}
+                        label="Email"
+                      />
 
 
-                    <Field
-                      name="password"
-                      type="password"
-                      component={renderField}
-                      label="Password"
-                    />
+                      <Field
+                        name="password"
+                        type="password"
+                        component={renderField}
+                        label="Password"
+                      />
 
-                    <Field
-                      name="passwordConfirm"
-                      type="password"
-                      component={renderField}
-                      label="Password Confirmation"
-                    />
+                      <Field
+                        name="passwordConfirm"
+                        type="password"
+                        component={renderField}
+                        label="Password Confirmation"
+                      />
 
-                    <div className="form-group">
-                      <div className="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
-                        <button type="button" className="btn btn-primary" onClick={ reset } disabled={ pristine || submitting }>Reset</button>
-                        <button type="submit" className="btn btn-success" disabled={ pristine || submitting }>Submit</button>
-                      </div>
-                   </div>
-              </form>
+                      <div className="form-group">
+                        <div className="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                          <button type="button" className="btn btn-primary" onClick={ reset } disabled={ pristine || submitting }>Reset</button>
+                          <button type="submit" className="btn btn-success" disabled={ pristine || submitting }>Submit</button>
+                        </div>
+                     </div>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 
-  handleFormSubmit(data) {
-    console.log("inside handleFormSubmit",data);
-    this.props.signup(data);
-  }
+    handleFormSubmit(data) {
+        console.log("inside handleFormSubmit",data);
+        this.props.signup(data);
+    }
 }
 
 function mapStateToProps(state) {
