b2a0ca1d7bfe76381467b1ed5a1194d464b2e64a
Progyan Bhattacharya <progyanb@acm.org>
2017-08-01 14:15:39 +0530

    ViewRole: Refactored codebase
    Fixed indentation with standard, removed inline function to separate one
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

--- src/components/ManageRoles/ViewRole.js
+++ src/components/ManageRoles/ViewRole.js
@@ -7,19 +7,15 @@ import { Link } from 'react-router';
 import { Label } from 'react-bootstrap';
 
 export default class ViewRole extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-      const { item } = this.props;
-      if (typeof item !== 'undefined' && item != null) {
-          return(
+    render() {
+        const { item } = this.props;
+        if (typeof item !== 'undefined' && item != null) {
+            return(
               <div className="x_panel_overflow x_panel tile fixed_height_320">
                 <div className="x_title role_label">
-                    <h2>{ item.role }
-                      <small>Role Details</small>
-                    </h2>
+                  <h2>{ item.role }
+                    <small>Role Details</small>
+                  </h2>
                   <ul className="nav navbar-right panel_toolbox">
                     <li>
                       <Link key={item.id} to={`/dashboard/manage-roles/add-roles?role=${item.role}`}>
@@ -32,41 +28,40 @@ export default class ViewRole extends Component {
                 <div className="x_content">
                   <div className="dashboard-widget-content">
                     <ul className="to_do">
-                      {
-                        item.components.map((comp, index) => {
-                          return(
-                            <li key={index}>
-                              <h4><i className="fa fa-support"></i> <Label bsStyle="primary">{comp.component}</Label></h4>
-                                {
-                                  comp.permissions.map((perm, index) => {
+                    {
+                        item.components.map((comp, index) => (
+                          <li key={index}>
+                            <h4>
+                              <i className="fa fa-support"></i>
+                              <Label bsStyle="primary">{comp.component}</Label>
+                            </h4>
+                            {
+                                comp.permissions.map((perm, index) => {
                                     let defaultChecked = null;
-                                    let permDisabled = null;
                                     if (perm.permission_id) {
-                                      defaultChecked = "checked";
-                                      permDisabled ="checked"
+                                        defaultChecked = "checked";
                                     }
                                     return(
                                         <div key={index}>
                                           <input
-                                            type="checkbox"
-                                            defaultChecked={defaultChecked}
-                                            disabled={true}/>
+                                           type="checkbox"
+                                           defaultChecked={defaultChecked}
+                                           disabled={true}/>
                                         <span className="perm_label">
                                           { perm.permission }
                                         </span>
                                       </div>
                                     );
-                                  })
-                                }
-                            </li>
-                          );
-                        })
-                      }
+                                })
+                            }
+                          </li>
+                        ))
+                    }
                     </ul>
                   </div>
                 </div>
               </div>
-          );
-      }
-  }
+            );
+        }
+    }
 }
